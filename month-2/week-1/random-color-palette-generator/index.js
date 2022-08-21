5b2785bfe0d497ffc48dcd86dfab79be346dda19
crimemaster007 <prachand9211@gmail.com>
2022-08-21 15:38:39 +0530

    random color palette generator added

---
+++ month-2/week-1/random-color-palette-generator/index.js
@@ -0,0 +1,45 @@
+const buttonCont = document.querySelector("#button-cont");
+const colorArr = document.querySelectorAll(".color-container");
+const colorParent = document.querySelectorAll(".color-parent");
+const notificationBar=document.querySelector(".notification-container")
+
+
+buttonCont.addEventListener("click", handleRandomColor);
+
+
+function handleRandomColor() {
+    // console.log(colorArr)
+    for (let i = 0; i < 4; i++){
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        colorArr[i].style.backgroundColor = "#" + randomColor;
+        let hexCode = colorParent[i].querySelector(".hex-code-container");
+        hexCode.innerText = "#" + randomColor;
+        
+    }
+}
+
+for (let i = 0; i < 4; i++){
+    colorParent[i].addEventListener("mouseover", (e) => {
+        let cpyContainer = colorParent[i].querySelector(".copy-container");
+        // console.log(cpyContainer);
+        cpyContainer.style.display = "block";
+    })
+    colorParent[i].addEventListener("mouseout", (e) => {
+        let cpyContainer = colorParent[i].querySelector(".copy-container");
+        // console.log(cpyContainer);
+        cpyContainer.style.display = "none";
+    })
+    colorParent[i].addEventListener("click", (e) => {
+        let colorVal = colorParent[i].querySelector(".hex-code-container").innerText;
+        document.body.style.backgroundColor = colorVal;
+        navigator.clipboard.writeText(colorVal);
+
+        setTimeout(() => {
+            notificationBar.innerText = "Color " + colorVal + " copied to your clipboard.";
+            notificationBar.style.display="block"
+        },1)
+        setTimeout(() => {
+            notificationBar.style.display="none"
+        },1000)
+    })
+}
